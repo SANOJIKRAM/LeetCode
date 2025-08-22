b3918ab25319151c407d4d5526d20fe5db97aab7
Sanoj Ikram H <sanojikram03@gmail.com>
2025-08-22 11:47:22 +0530

    Time: 0 ms (100%), Space: 41.3 MB (96.2%) - LeetHub

--- 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.java
@@ -2,8 +2,8 @@ class Solution {
     public String replaceDigits(String s) {
         char[] arr = s.toCharArray();
         for (int i = 1; i < arr.length; i += 2) {
-            int shift = arr[i] - '0'; // convert digit char to int
-            arr[i] = (char) (arr[i - 1] + shift); // shift previous char
+            int shift = arr[i] - '0'; 
+            arr[i] = (char) (arr[i - 1] + shift); 
         }
         return new String(arr);
     }
