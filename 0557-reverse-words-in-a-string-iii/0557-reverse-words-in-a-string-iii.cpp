76ad9188be0042db0cac4c28090b605cf3864399
Sanoj Ikram H <sanojikram03@gmail.com>
2025-09-02 14:04:22 +0530

    Time: 2 ms (58.45%), Space: 12.2 MB (76.33%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0, n = s.size();
+        for (int end = 0; end <= n; ++end) {
+            if (end == n || s[end] == ' ') {
+                reverse(s.begin() + start, s.begin() + end);
+                start = end + 1;
+            }
+        }
+        return s;
+    }
+};
