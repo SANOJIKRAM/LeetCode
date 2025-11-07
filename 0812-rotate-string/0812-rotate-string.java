fc4557f49b5e6910c6fbc882d3e570aabbca08ee
Sanoj Ikram H <sanojikram03@gmail.com>
2025-11-07 12:26:05 +0530

    Time: 0 ms (100%), Space: 42.6 MB (6.19%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String doubledString = s + s;
+        return doubledString.contains(goal);
+    }
+}
